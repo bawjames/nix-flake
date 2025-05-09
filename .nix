ac973ef01c0ff731b148022765bae36902f01ded
James Baw <jamiebaw@gmail.com>
2025-05-09 02:32:54 +0100

    updated .gitignore

--- .nix
+++
@@ -1,8 +0,0 @@
-with lib; rec {
-  files = builtins.readDir ./.;
-  isDir = files attrsets.filterAttrs (_: type:  type == "directory") files;
-  isNix = str: strings.hasSuffix ".nix" str;
-  paths = str: ./. + str;
-  exclude = [ ./configuration.nix ];
-  notExcluded = path: lists.elem path exclude;
-}
